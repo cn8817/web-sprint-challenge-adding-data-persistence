ed0174385d655a4fde0ecb661a256cf14adfffc1
cn8817 <christinenguyen1433@gmail.com>
2021-09-10 00:35:06 -0700

    debugging task

--- api/task/model.js
+++ api/task/model.js
@@ -5,6 +5,7 @@ function getTasks() {
     return db('tasks as t')
     .leftJoin('projects as p', 'p.projects_id', 't.projects_id')
     .select('t.task_id', 't.task_description', 't.task_notes', 't.task_completed', 'p.project_id')
+    .where('t.task_id', task_id)
 }
 
 async function createTask(task) {
