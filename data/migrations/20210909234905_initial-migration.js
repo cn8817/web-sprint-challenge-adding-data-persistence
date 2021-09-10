00c18a25bbbf74627f033e04d593067c09d39de6
cn8817 <christinenguyen1433@gmail.com>
2021-09-09 17:27:59 -0700

    completed migration tables

--- data/migrations/20210909234905_initial-migration.js
+++ data/migrations/20210909234905_initial-migration.js
@@ -2,16 +2,42 @@
 exports.up = async function(knex) {
   await knex.schema
     .createTable('projects', tbl => {
-
+        tbl.increments('project_id')
+        tbl.string('project_name', 128).notNullable().unique()
+        tbl.string('project_description', 128)
+        tbl.boolean('project_completed')
     })
     .createTable('resources', tbl => {
-        
+        tbl.increments('resource_id')
+        tbl.string('resource_name', 128).notNullable()
+        tbl.string('resource_description', 128)
     })
     .createTable('tasks', tbl => {
-        
+        tbl.increments('task_id')
+        tbl.string('task_description', 128).notNullable()
+        tbl.string('task_notes', 128)
+        tbl.boolean('task_completed')
+        tbl.integer('project_id')
+            .unsigned()
+            .references('project_id')
+            .inTable('projects')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
     })
     .createTable('project_resources', tbl => {
-        
+        tbl.increments('project_resources_id')
+        tbl.integer('project_id')
+            .unsigned()
+            .references('project_id')
+            .inTable('projects')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
+        tbl.integer('resource_id')
+            .unsigned()
+            .references('resource_id')
+            .inTable('resources')
+            .onDelete('RESTRICT')
+            .onUpdate('RESTRICT')
     })
 };
 
